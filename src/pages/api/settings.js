2f09682608ef47c5cbf96429f3c4e19d10de6af8
Sarvajeeth <sarvajeeth21416@iiitd.ac.in>
2023-11-30 00:23:36 +0530

    Corrected the Settings API

--- src/pages/api/settings.js
+++ src/pages/api/settings.js
@@ -1,29 +1,49 @@
 import { connectToDatabase } from './mongodb.mjs';
 
 export default async function handler(req, res) {
-
     if (req.method === 'POST') {
-        console.log(req.body)
-        console.log("Ho gaya start")
         try {
-             
+            const {
+                userEmail,
+                vegNonVeg,
+                height,
+                healthIssues,
+                weight,
+                favourite_regions,
+                AllergicTo,
+            } = req.body;
 
             const client = await connectToDatabase();
             const collection = client.collection('User');
 
             const user = await collection.findOne({ email: userEmail });
+
             if (user) {
-                console.log("FOunddd")
+                console.log("Found");
+
+                const existingFavouriteRegions = user.favourite_regions || [];
+                const existingAllergicTo = user.AllergicTo || [];
+
+                const updatedFavouriteRegions = [...existingFavouriteRegions, ...favourite_regions];
+                const updatedAllergicTo = [...existingAllergicTo, ...AllergicTo];
+
                 await collection.updateOne({ email: userEmail }, {
                     $set: {
-                        ...req.body,
+                        vegNonVeg,
+                        height,
+                        healthIssues,
+                        weight,
+                        favourite_regions: updatedFavouriteRegions,
+                        AllergicTo: updatedAllergicTo,
                     },
                 });
+
                 res.status(200).json({ message: 'User details updated' });
             } else {
                 res.status(404).json({ message: 'User not found' });
             }
         } catch (error) {
+            console.error("Error:", error);
             res.status(500).json({ message: 'Error saving data to the database' });
         }
     } else {
